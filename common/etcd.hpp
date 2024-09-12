a61a682fab67569854a02d29fd67ab2477cb1413
linzhiyouyu <chase3332379811@gmail.com>
2024-09-12 12:54:29 +0800

    通用头文件

--- common/etcd.hpp
+++ common/etcd.hpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <functional>
 
+using namespace lcz_zpy;
 //服务注册客户端类
 class Registry {
 public:
@@ -20,9 +21,9 @@ public:
     {}
     ~Registry() { _keep_alive->Cancel(); }
     bool registry(const std::string& key, const std::string& val) {
-        auto resp = _client->put("/service/user", "127.0.0.1:8081", _lease_id).get();
+        auto resp = _client->put(key, val, _lease_id).get();
         if(resp.is_ok() == false) {
-            ERROR("注册数据失败: {}", /*resp.error_message()*/"你好哈");
+            DEBUG("注册数据失败: {}", resp.error_message());
             return false;
         }
         return true;
@@ -33,49 +34,49 @@ private:
     uint64_t  _lease_id;    //租约id
 };
 
-// //服务发现客户端类
-// class Discovery {
-// public:
-//     using ptr = std::shared_ptr<Discovery>;
-//     using notify = std::function<void(std::string, std::string)>;
-//     Discovery(const std::string& host, const std::string& basedir, const notify& put_cb, const notify& del_cb)
-//         :_client(std::make_shared<etcd::Client>(host))
-//         ,_put_cb(put_cb)
-//         ,_del_cb(del_cb) {
+//服务发现客户端类
+class Discovery {
+public:
+    using ptr = std::shared_ptr<Discovery>;
+    using notify = std::function<void(std::string, std::string)>;
+    Discovery(const std::string& host, const std::string& basedir, const notify& put_cb, const notify& del_cb)
+        :_client(std::make_shared<etcd::Client>(host))
+        ,_put_cb(put_cb)
+        ,_del_cb(del_cb) {
         
-//         //1. 进行服务发现
-//         auto resp = _client->ls(basedir).get();
-//         if(resp.is_ok() == false) {
-//             ERROR("获取服务信息数据失败: {}", resp.error_message());
-//         }
-//         int sz = resp.keys().size();
-//         for(int i = 0; i < sz; ++i)
-//             if(_put_cb)
-//                 _put_cb(resp.key(i), resp.value(i).as_string());
-//         //2. 然后进行事件监控，监控数据发生的改变并调用对应回调来处理 
-//         //_watcher = std::make_shared<etcd::Watcher>(*_client.get(), basedir, std::bind(&Discovery::callback, std::placeholders::_1), true);
-//     }
-// private:
-//     void callback(const etcd::Response& resp) {
-//         if(resp.is_ok() == false) {
-//             ERROR("收到一个错误的事件通知: {}", resp.error_message());
-//             return;
-//         }
-//         for(auto&& ev : resp.events()) {
-//             if(ev.event_type() == etcd::Event::EventType::PUT) {
-//                 if(_put_cb)
-//                     _put_cb(ev.kv().key(), ev.kv().as_string());
-//                 DEBUG("新增服务: {}-{}", ev.kv().key(), ev.kv().as_string());
-//             } else if(ev.event_type() == etcd::Event::EventType::DELETE_) { 
-//                 if(_del_cb)
-//                     _del_cb(ev.prev_kv().key(), ev.prev_kv().as_string());
-//                 DEBUG("下线服务: {}-{}", ev.prev_kv().key(), ev.prev_kv().as_string());
-//             }
-//         }
-//     }
-// private:
-//     notify _put_cb;
-//     notify _del_cb;
-//     std::shared_ptr<etcd::Client> _client;
-//     std::shared_ptr<etcd::Watcher> _watcher;
-// };
+        //1. 进行服务发现
+        auto resp = _client->ls(basedir).get();
+        if(resp.is_ok() == false) {
+            DEBUG("获取服务信息数据失败: {}", resp.error_message());
+        }
+        int sz = resp.keys().size();
+        for(int i = 0; i < sz; ++i)
+            if(_put_cb)
+                _put_cb(resp.key(i), resp.value(i).as_string());
+        //2. 然后进行事件监控，监控数据发生的改变并调用对应回调来处理 
+        //_watcher = std::make_shared<etcd::Watcher>(*_client.get(), basedir, std::bind(&Discovery::callback, std::placeholders::_1), true);
+    }
+private:
+    // void callback(const etcd::Response& resp) {
+    //     if(resp.is_ok() == false) {
+    //         ERROR("收到一个错误的事件通知: {}", resp.error_message());
+    //         return;
+    //     }
+    //     for(auto&& ev : resp.events()) {
+    //         if(ev.event_type() == etcd::Event::EventType::PUT) {
+    //             if(_put_cb)
+    //                 _put_cb(ev.kv().key(), ev.kv().as_string());
+    //             DEBUG("新增服务: {}-{}", ev.kv().key(), ev.kv().as_string());
+    //         } else if(ev.event_type() == etcd::Event::EventType::DELETE_) { 
+    //             if(_del_cb)
+    //                 _del_cb(ev.prev_kv().key(), ev.prev_kv().as_string());
+    //             DEBUG("下线服务: {}-{}", ev.prev_kv().key(), ev.prev_kv().as_string());
+    //         }
+    //     }
+    // }
+private:
+    notify _put_cb;
+    notify _del_cb;
+    std::shared_ptr<etcd::Client> _client;
+    std::shared_ptr<etcd::Watcher> _watcher;
+};
--- common/logger.hpp
+++ common/logger.hpp
@@ -6,7 +6,7 @@
 #include <iostream>
 
 //mode: true - 发布模式  false - 调试模式
-namespace perfect {
+namespace lcz_zpy {
     #define LOG_NAME "logger"
     #define LOG_FILE "log/runing.log"
     std::shared_ptr<spdlog::logger> global_logger;
@@ -14,24 +14,26 @@ namespace perfect {
     class StartupInitlialize {
         public:
             StartupInitlialize() {
-                //初始化异步线程数量和日志缓冲区节点数量
+                // 初始化异步线程数量和日志缓冲区节点数量
                 spdlog::init_thread_pool(32768, 1);
-                //构造日志器
+                // 构造日志器
                 spdlog::flush_every(std::chrono::seconds(1));
                 spdlog::flush_on(spdlog::level::debug);
                 global_logger = spdlog::basic_logger_mt(LOG_NAME, LOG_FILE);
-                //global_logger = spdlog::stdout_color_mt(LOG_NAME);
-                //设置日志输出等级
+                // 设置日志输出等级
                 global_logger->set_level(spdlog::level::debug);
-                //设置日志输出格式
+                // 设置日志输出格式
                 global_logger->set_pattern("%H:%M:%S [%l] %v");
             }
     };
+
+    // 全局静态实例，保证日志在启动时初始化
     static StartupInitlialize start_initialize;
 
-    #define DEBUG(format, ...) perfect::global_logger->debug(std::string("{}:{} ") + format, __FILE__, __LINE__, ##__VA_ARGS__);
-    #define INFO(format, ...) perfect::global_logger->info(std::string("{}:{} ") + format, __FILE__, __LINE__, ##__VA_ARGS__);
-    #define WARN(format, ...) perfect::global_logger->warn(std::string("{}:{} ") + format, __FILE__, __LINE__, ##__VA_ARGS__);
-    #define ERROR(format, ...) perfect::global_logger->error(std::string("{}:{} ") + format, __FILE__, __LINE__, ##__VA_ARGS__);
-    #define CRITICAL(format, ...) perfect::global_logger->critical(std::string("{}:{} ") + format, __FILE__, __LINE__, ##__VA_ARGS__);
+    // 修改宏定义，直接使用 spdlog 的格式化功能
+    #define DEBUG(format, ...) lcz_zpy::global_logger->debug("{}:{} " format, __FILE__, __LINE__, ##__VA_ARGS__);
+    #define INFO(format, ...) lcz_zpy::global_logger->info("{}:{} " format, __FILE__, __LINE__, ##__VA_ARGS__);
+    #define WARN(format, ...) lcz_zpy::global_logger->warn("{}:{} " format, __FILE__, __LINE__, ##__VA_ARGS__);
+    #define ERROR(format, ...) lcz_zpy::global_logger->error("{}:{} " format, __FILE__, __LINE__, ##__VA_ARGS__);
+    #define CRITICAL(format, ...) lcz_zpy::global_logger->critical("{}:{} " format, __FILE__, __LINE__, ##__VA_ARGS__);
 }
