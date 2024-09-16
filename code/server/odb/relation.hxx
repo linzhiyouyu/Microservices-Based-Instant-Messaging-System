c24337d58ce78e7d0ab627dcb378508feba26ecf
linzhiyouyu <chase3332379811@gmail.com>
2024-09-16 22:42:58 +0800

    数据库表构建

--- code/server/odb/relation.hxx
+++ code/server/odb/relation.hxx
@@ -1,3 +1,4 @@
+#pragma once
 #include <cstddef>
 #include <odb/nullable.hxx>
 #include <odb/core.hxx>
@@ -6,11 +7,12 @@ namespace chat_im {
     #pragma db object table("relation")
     class Relation {
     public:
+        Relation() {}
         Relation(const std::string& uid, const std::string& pid)
             :_user_id(uid), _peer_id(pid) {}
         std::string user_id() const { return _user_id; }
         void user_id(const std::string& uid) { _user_id = uid; }
-        std::string _peer_id() const { return _peer_id; }
+        std::string peer_id() const { return _peer_id; }
         void peer_id(const std::string& pid) { _peer_id = pid; }
     private:
         friend class odb::access ;
